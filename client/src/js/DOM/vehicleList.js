e48188da2a283c43ad6455b6b898111386e72686
John8190 <joaovinicius1234576@gmail.com>
2023-05-03 16:42:29 +0100

    changes

--- client/src/js/DOM/vehicleList.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener("click", (e) => {
-  const target = e.target.closest("#vehicle-link");
-  const targetAtt = target.getAttribute("href");
-
-  location.assign(targetAtt);
-  location.reload();
-});
